630ff4134154f73d6b269f7300d90abde33e8bb3
shiv160419 <129748540+shiv160419@users.noreply.github.com>
2023-04-03 19:14:43 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/exp1/sw.js', {scope: '/exp1/'})
+  
+  
+  
+  
+  
     .then(reg => console.log('service worker registered'))
     .catch(err => console.log('service worker not registered', err));
 }
